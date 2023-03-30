760ed80aff07f6f4c985902b4d6bc0d12f4f469a
Denny Setyawan Huda <dennshgit@gmail.com>
2023-03-30 16:21:26 +0700

    chore: minimum pasword 6

--- src/validations/LoginValidation.ts
+++ src/validations/LoginValidation.ts
@@ -2,5 +2,5 @@ import * as yup from "yup";
 
 export const loginSchema = yup.object({
   email: yup.string().email().required("required"),
-  password: yup.string().min(5).required("required"),
+  password: yup.string().min(6).required("required"),
 });
