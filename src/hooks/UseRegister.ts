040c2dfbfc8f47d25000e4330762d39957fa04d1
Denny Setyawan Huda <dennshgit@gmail.com>
2023-03-30 12:40:31 +0700

    feat: add image upload

--- src/hooks/UseRegister.ts
+++ src/hooks/UseRegister.ts
@@ -1,14 +1,24 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase/config";
+import { auth, storage } from "../firebase/config";
+import { ref, uploadBytes } from "firebase/storage";
 
 export default function useRegister() {
-  async function register(email: string, password: string) {
+  async function register(email: string, password: string, image: File | null) {
     try {
       const response = await createUserWithEmailAndPassword(
         auth,
         email,
         password
       );
+
+      if (image) {
+        const imagePath = `images/${response.user.uid}/${image.name}`;
+        const refStorage = ref(storage, imagePath);
+        await uploadBytes(refStorage, image);
+      } else {
+        console.log("sorry file not found");
+      }
+
       console.log(response);
     } catch (error) {
       console.log(error);
