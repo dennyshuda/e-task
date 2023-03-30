3d6dc14bea9a9c3025c2de0828e2261a4088e4c7
Denny Setyawan Huda <dennshgit@gmail.com>
2023-03-31 00:19:55 +0700

    feat: function update online true when user login

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,15 +1,16 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase/config";
+import { auth, db } from "../firebase/config";
 import { useDispatch, useSelector } from "react-redux";
 import { userLogin } from "../features/userSlice";
 import { useNavigate } from "react-router";
+import { doc, updateDoc } from "firebase/firestore";
 
 export default function useLogin() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   function login(email: string, password: string) {
     signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
+      .then(async (userCredential) => {
         const user = userCredential.user;
         console.log(user);
         const userDetail = {
@@ -18,6 +19,11 @@ export default function useLogin() {
           photoURL: user.photoURL,
           email: user.email,
         };
+
+        await updateDoc(doc(db, "users", user.uid), {
+          online: true,
+        });
+
         dispatch(userLogin(userDetail));
         navigate("/");
       })
