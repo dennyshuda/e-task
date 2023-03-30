4158cd5d626e6f842e294e9ec0ff7e86dc3a0144
Denny Setyawan Huda <dennshgit@gmail.com>
2023-03-31 00:22:28 +0700

    feat: show sidebar and header when user login

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,9 +1,11 @@
 import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
 import { Children } from "../types";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
 
 export default function RootLayout({ children }: Children) {
-  const auth = false;
+  const auth = useSelector((state: RootState) => state.user);
 
   return (
     <div className="flex w-full h-screen">
