46c208bdb635958c38b3c381e24c90708614f97d
Denny Setyawan Huda <dennshgit@gmail.com>
2023-03-30 13:06:24 +0700

    feat: update display name and image url from storage

--- src/hooks/UseRegister.ts
+++ src/hooks/UseRegister.ts
@@ -1,9 +1,14 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, storage } from "../firebase/config";
-import { ref, uploadBytes } from "firebase/storage";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 export default function useRegister() {
-  async function register(email: string, password: string, image: File | null) {
+  async function register(
+    email: string,
+    password: string,
+    image: File | null,
+    displayName: string
+  ) {
     try {
       const response = await createUserWithEmailAndPassword(
         auth,
@@ -15,6 +20,17 @@ export default function useRegister() {
         const imagePath = `images/${response.user.uid}/${image.name}`;
         const refStorage = ref(storage, imagePath);
         await uploadBytes(refStorage, image);
+
+        await getDownloadURL(refStorage)
+          .then(async (url) => {
+            await updateProfile(response.user, {
+              displayName: displayName,
+              photoURL: url,
+            });
+          })
+          .catch((err) => console.log(err));
+
+        console.log(response.user);
       } else {
         console.log("sorry file not found");
       }
