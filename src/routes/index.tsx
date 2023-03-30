ce77db733e901ac394fe091269543f325ec9ad1c
Denny Setyawan Huda <dennshgit@gmail.com>
2023-03-31 00:21:39 +0700

    feat: separate private route and protected route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,17 +6,23 @@ import Users from "../pages/Users";
 import DetailTask from "../pages/DetailTask";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import PrivateRoute from "./PrivateRoute";
+import ProtectedRoute from "./ProtectedRoute";
 
 export default function SetupRouter() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/my-task" element={<MyTask />} />
-      <Route path="/add-task" element={<AddTask />} />
-      <Route path="/detail/:id" element={<DetailTask />} />
-      <Route path="/users" element={<Users />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="register" element={<Register />} />
+      <Route element={<PrivateRoute />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/my-task" element={<MyTask />} />
+        <Route path="/add-task" element={<AddTask />} />
+        <Route path="/detail/:id" element={<DetailTask />} />
+        <Route path="/users" element={<Users />} />
+      </Route>
+      <Route element={<ProtectedRoute />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
     </Routes>
   );
 }
