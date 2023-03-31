6e2a9f155a4f40fa5595fe76b9035d1637d132e6
Denny Setyawan Huda <dennshgit@gmail.com>
2023-03-31 14:36:56 +0700

    feat: add react select form users and category

--- src/pages/AddTask.tsx
+++ src/pages/AddTask.tsx
@@ -1,4 +1,35 @@
+import Select from "react-select";
+import useGetUsers from "../hooks/useGetUsers";
+import { useEffect, useState } from "react";
+import { UserType } from "../types";
+
+interface OptionUsers {
+  value: UserType;
+  label: string;
+}
+
 export default function AddTask() {
+  const [optionUsers, setOptionUsers] = useState<OptionUsers[]>();
+  const { users } = useGetUsers();
+
+  useEffect(() => {
+    if (users) {
+      const option = users.map((user) => {
+        return {
+          value: user,
+          label: user.displayName,
+        };
+      });
+      setOptionUsers(option);
+    }
+  }, [users]);
+
+  const options = [
+    { value: "urgent", label: "Urgent" },
+    { value: "important", label: "Important" },
+    { value: "help", label: "Help" },
+  ];
+
   return (
     <div>
       <h1 className="text-2xl font-medium">Add Task</h1>
@@ -38,6 +69,20 @@ export default function AddTask() {
             name="duedate"
             className="outline-none border-[1px] self-start py-1 px-2 rounded-md"
           />
+          <span className="error-message">Error</span>
+        </div>
+        <div className="mb-3 flex flex-col">
+          <label htmlFor="duedate" className="mb-2 font-medium">
+            User
+          </label>
+          <Select options={optionUsers} isMulti />
+          <span className="error-message">Error</span>
+        </div>
+        <div className="mb-3 flex flex-col">
+          <label htmlFor="duedate" className="mb-2 font-medium">
+            Category
+          </label>
+          <Select options={options} />
           <span className="error-message">Error</span>
         </div>
         <button className="bg-blue-700 hover:bg-blue-800 btn">Add Task</button>
