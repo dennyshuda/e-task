ab440d1e599ec935c513ee52f6c20afa92c0de7f
Denny Setyawan Huda <dennshgit@gmail.com>
2023-04-03 03:41:26 +0700

    feat: compare tasks

---
+++ src/hooks/useCompare.ts
@@ -0,0 +1,13 @@
+import { TasksType } from "../types";
+
+export default function useCompare() {
+  const isComplete = (task: TasksType) => task.complete;
+
+  const isInWork = (task: TasksType) =>
+    new Date(task.duedate).getTime() < new Date().getTime() && !task.complete;
+
+  const isDue = (task: TasksType) =>
+    new Date(task.duedate).getTime() > new Date().getTime() && !task.complete;
+
+  return { isComplete, isInWork, isDue };
+}
