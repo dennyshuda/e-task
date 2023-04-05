1a5712b39f36ede898a8ca5b1e7a829d4fecce56
Denny Setyawan Huda <dennshgit@gmail.com>
2023-04-06 01:35:49 +0700

    fix: change compare sign

--- src/hooks/useCompare.ts
+++ src/hooks/useCompare.ts
@@ -4,10 +4,10 @@ export default function useCompare() {
   const isComplete = (task: TasksType) => task.complete;
 
   const isInWork = (task: TasksType) =>
-    new Date(task.duedate).getTime() < new Date().getTime() && !task.complete;
+    new Date(task.duedate).getTime() > new Date().getTime() && !task.complete;
 
   const isDue = (task: TasksType) =>
-    new Date(task.duedate).getTime() > new Date().getTime() && !task.complete;
+    new Date(task.duedate).getTime() < new Date().getTime() && !task.complete;
 
   return { isComplete, isInWork, isDue };
 }
